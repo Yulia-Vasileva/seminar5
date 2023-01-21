5ddbc48f2149b6f7935a0d202a78e079b73e162a
Yulia <vasilevaa93@mail.ru>
2023-01-21 07:24:38 +0300

    задача38

---
+++ задача38/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задайте массив натуральных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+int[]array ={9,100,2,7,43};
+
+int max=array[0];
+int min=array[0];
+for (int i = 0; i < array.Length; i++)
+{
+if(array[i]>max)
+{
+    max=array[i];
+}
+else if(array[i]<min)
+{
+    min=array[i];
+}
+}
+Console.WriteLine ($"Максимальное значение массива = {max}");
+Console.WriteLine ($"Минимальное значение массива = {min}");
+
+int difference=max-min;
+
+Console.WriteLine ($"Разница = {difference}");
